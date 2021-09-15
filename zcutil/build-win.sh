3d4bbef8f65137da5d654079c9df3f1a0e19426f
dimxy <dimxy@komodoplatform.com>
2021-09-15 12:37:19 +0500

    tokel chain params added in build.sh

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -24,6 +24,10 @@ cd ../
 
 ./autogen.sh
 CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread" ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared
-sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
+sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure \
+  --with-custom-bin=yes CUSTOM_BIN_NAME=tokel CUSTOM_BRAND_NAME=Tokel \
+  CUSTOM_SERVER_ARGS="'-ac_name=TOKEL -ac_supply=100000000 -ac_eras=2 -ac_cbmaturity=1 -ac_reward=100000000,4250000000 -ac_end=80640,0 -ac_decay=0,77700000 -ac_halving=0,525600 -ac_cc=555 -ac_ccenable=236,245,246,247 -ac_adaptivepow=6 -addnode=135.125.204.169 -addnode=192.99.71.125 -nspv_msg=1'" \
+  CUSTOM_CLIENT_ARGS='-ac_name=TOKEL'
+  
 cd src/
 CC="${CC} -g " CXX="${CXX} -g " make V=1  komodod.exe komodo-cli.exe komodo-tx.exe

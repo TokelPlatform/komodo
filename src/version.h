e61b1786f3685bafaef15c7b2cf6d69f12082242
CHMEX <33424502+CHMEX@users.noreply.github.com>
2024-09-12 15:45:36 +0200

    Update version.h

--- src/version.h
+++ src/version.h
@@ -33,8 +33,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 170009 + 3; // Tokel HF October 2024
-static const int STAKEDMIN_PEER_PROTO_VERSION = 170009 + 3; // Tokel HF October 2024
+static const int MIN_PEER_PROTO_VERSION = 170009; // Tokel HF October 2024 after hardfork update?
+static const int STAKEDMIN_PEER_PROTO_VERSION = 170009; // Tokel HF October 2024 after hardfork update?
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this

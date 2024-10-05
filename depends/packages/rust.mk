d7d13c2035fe2d0368f685860393d673bbf280e4
ComputerGenie <tim@yourcomputergenie.com>
2024-10-05 17:02:18 -0500

    Update depends
    Closes #54, closes #55

--- depends/packages/rust.mk
+++ depends/packages/rust.mk
@@ -9,49 +9,50 @@ $(package)_sha256_hash_darwin=9818dab2c3726d63dfbfde12c9273e62e484ef6d6f6e05a643
 $(package)_file_name_mingw32=rust-$($(package)_version)-x86_64-pc-windows-gnu.tar.gz
 $(package)_sha256_hash_mingw32=9a2a887defb9d0e0aa48fd54237b583a693a2565420b4d90efd0740c0fe69b0d
 
+define rust_target
+$(if $($(1)_rust_target_$(2)),$($(1)_rust_target_$(2)),$(if $(findstring darwin,$(3)),x86_64-apple-darwin,$(if $(findstring freebsd,$(3)),x86_64-unknown-freebsd,$(2))))
+endef
+
 define $(package)_set_vars
 $(package)_stage_opts=--disable-ldconfig
 $(package)_stage_build_opts=--without=rust-docs-json-preview,rust-docs
 endef
 
-ifeq ($(build_os),darwin)
-$(package)_file_name=$($(package)_file_name_darwin)
-$(package)_sha256_hash=$($(package)_sha256_hash_darwin)
-else ifeq ($(host_os),mingw32)
-$(package)_file_name=$($(package)_file_name_mingw32)
-$(package)_sha256_hash=$($(package)_sha256_hash_mingw32)
-else
-$(package)_file_name=$($(package)_file_name_linux)
-$(package)_sha256_hash=$($(package)_sha256_hash_linux)
-endif
-
-ifeq ($(host_os),mingw32)
+ifneq ($(canonical_host),$(build))
+$(package)_rust_target=$(call rust_target,$(package),$(canonical_host),$(host_os))
+$(package)_exact_file_name=rust-std-$($(package)_version)-$($(package)_rust_target).tar.gz
+$(package)_exact_sha256_hash=$($(package)_rust_std_sha256_hash_$($(package)_rust_target))
 $(package)_build_subdir=buildos
-$(package)_extra_sources = $($(package)_file_name_$(build_os))
+$(package)_extra_sources=$($(package)_file_name_$(build_os))
 
 define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_file_name_$(build_os)),$($(package)_file_name_$(build_os)),$($(package)_sha256_hash_$(build_os)))
 endef
 
+# Runs from: build dir
 define $(package)_extract_cmds
   mkdir -p $($(package)_extract_dir) && \
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_sha256_hash_$(build_os))  $($(package)_source_dir)/$($(package)_file_name_$(build_os))" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
-  mkdir mingw32 && \
-  tar --strip-components=1 -xf $($(package)_source) -C mingw32 && \
+  mkdir $(canonical_host) && \
+  tar --strip-components=1 -xf $($(package)_source) -C $(canonical_host) && \
   mkdir buildos && \
-  tar --strip-components=1 -xf $($(package)_source_dir)/$($(package)_file_name_$(build_os)) -C buildos
+  tar --strip-components=1 -xf $($(package)_source_dir)/$($(package)_file_name_$(build_os)) -C buildos && \
+  echo "$(package) for $(build): $($(package)_staging_dir)/buildos" && \
+  echo "$(package) for $(canonical_host): $($(package)_staging_dir)/$(canonical_host)"
 endef
 
+# Runs from: build dir/$(package)_build_subdir
+# first install.sh is from buildos for native build_os platform, second it for host_os (target) platform
 define $(package)_stage_cmds
-  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native $($(package)_stage_opts) $($(package)_stage_build_opts) && \
-  cp -r ../mingw32/rust-std-x86_64-pc-windows-gnu/lib/rustlib/x86_64-pc-windows-gnu $($(package)_staging_dir)$(host_prefix)/native/lib/rustlib
+  bash ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(build_prefix) $($(package)_stage_opts) $($(package)_stage_build_opts) && \
+  ../$(canonical_host)/install.sh --destdir=$($(package)_staging_dir) --prefix=$(build_prefix) $($(package)_stage_opts)
 endef
 else
-
+# for normal compilation (non-cross)
 define $(package)_stage_cmds
-  ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(host_prefix)/native $($(package)_stage_opts) $($(package)_stage_build_opts)
+  bash ./install.sh --destdir=$($(package)_staging_dir) --prefix=$(build_prefix) $($(package)_stage_opts) $($(package)_stage_build_opts)
 endef
 endif
